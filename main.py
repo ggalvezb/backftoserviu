23476bc10ca26539104b4c37bd8e848dc3306b3f
Gonzalo Gálvez Bobadilla <ggalvezb@minvu.cl>
2025-02-18 13:53:21 -0300

    Cors agregado

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from fastapi import FastAPI
 from routers import FTO
 from routers import Proyecto
 from routers import Administrador
+from fastapi.middleware.cors import CORSMiddleware
 # Inicia el servidor: uvicorn main:app --reload
 
 app=FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar dominios en lugar de "*" para mayor seguridad
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados HTTP
+)
 
 #routers
 app.include_router(FTO.router)
